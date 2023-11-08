49ecfd1f23416b0b4e53dd17214dc15081f4f89a
Wngmaker <141830764+Wngmaker@users.noreply.github.com>
2023-11-08 18:25:56 +0900

    Add files via upload

---
+++ module02/baivenhass6/src/ss1/Main.java
@@ -0,0 +1,68 @@
+package ss1
+class Circle {
+    private double radius;
+    private String color;
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
+
+class Cylinder extends Circle {
+    private double height;
+
+    public Cylinder(double radius, String color, double height) {
+        super(radius, color);
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getVolume() {
+        return getArea() * height;
+    }
+
+    @Override
+    public String toString() {
+        return "Cylinder{" +
+                "radius=" + getRadius() +
+                ", color='" + getColor() + '\'' +
+                ", height=" + height +
+                '}';
+    }
+}
